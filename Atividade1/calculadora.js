164373f7d2773fad2ba6b2c76aa4962c7019bb55
Alisson Guilherme <alisson.formento.tj@gmail.com>
2024-02-05 21:39:43 -0300

    Primeiro aplicativo: Calculadora

---
+++ Atividade1/calculadora.js
@@ -0,0 +1,51 @@
+let tela = document.getElementById('tela')
+
+
+function inserir0(params) {
+    
+}
+function inserir1(params) {
+    
+}
+function inserir2(params) {
+    
+}
+function inserir3(params) {
+    
+}
+function inserir4(params) {
+    
+}
+function inserir5(params) {
+    
+}
+function inserir6(params) {
+    
+}
+function inserir7(params) {
+    
+}
+function inserir8(params) {
+    
+}
+function inserir9(params) {
+    
+}
+function inserir(params) {
+    
+}
+function inserir(params) {
+    
+}
+function inserir(params) {
+    
+}
+function inserir(params) {
+    
+}
+function inserir(params) {
+    
+}
+function inserir(params) {
+    
+}
