3d199f7553279266acba89fbb7e29c7767002126
Alisson Guilherme <alisson.formento.tj@gmail.com>
2024-03-05 17:54:23 -0300

    formatação do Javascript

--- Atividade1/calculadora.js
+++ Atividade1/calculadora.js
@@ -1,51 +1,83 @@
 let tela = document.getElementById('tela')
 
-
-function inserir0(params) {
-    
-}
-function inserir1(params) {
-    
-}
-function inserir2(params) {
-    
-}
-function inserir3(params) {
-    
-}
-function inserir4(params) {
-    
-}
-function inserir5(params) {
-    
-}
-function inserir6(params) {
-    
-}
-function inserir7(params) {
-    
-}
-function inserir8(params) {
-    
-}
-function inserir9(params) {
-    
-}
-function inserir(params) {
-    
-}
-function inserir(params) {
-    
-}
-function inserir(params) {
-    
-}
-function inserir(params) {
-    
-}
-function inserir(params) {
-    
-}
-function inserir(params) {
-    
-}
+let dadosDaTela = []
+inserir0.addEventListener('click', function(){ 
+    dadosDaTela.push(0)
+    tela.innerHTML = dadosDaTela
+    console.log(dadosDaTela)
+})
+inserir1.addEventListener('click', function(){ 
+    dadosDaTela.push(0)
+    tela.innerHTML = dadosDaTela
+    console.log(dadosDaTela)
+})
+inserir2.addEventListener('click', function(){ 
+    dadosDaTela.push(0)
+    tela.innerHTML = dadosDaTela
+    console.log(dadosDaTela)
+})
+inserir3.addEventListener('click', function(){ 
+    dadosDaTela.push(0)
+    tela.innerHTML = dadosDaTela
+    console.log(dadosDaTela)
+})
+inserir4.addEventListener('click', function(){ 
+    dadosDaTela.push(0)
+    tela.innerHTML = dadosDaTela
+    console.log(dadosDaTela)
+})
+inserir5.addEventListener('click', function(){ 
+    dadosDaTela.push(0)
+    tela.innerHTML = dadosDaTela
+    console.log(dadosDaTela)
+})
+inserir6.addEventListener('click', function(){ 
+    dadosDaTela.push(0)
+    tela.innerHTML = dadosDaTela
+    console.log(dadosDaTela)
+})
+inserir7.addEventListener('click', function(){ 
+    dadosDaTela.push(0)
+    tela.innerHTML = dadosDaTela
+    console.log(dadosDaTela)
+})
+inserir8.addEventListener('click', function(){ 
+    dadosDaTela.push(0)
+    tela.innerHTML = dadosDaTela
+    console.log(dadosDaTela)
+})
+inserir9.addEventListener('click', function(){ 
+    dadosDaTela.push(0)
+    tela.innerHTML = dadosDaTela
+    console.log(dadosDaTela)
+})
+inserirDivisao.addEventListener('click', function(){ 
+    dadosDaTela.push(0)
+    tela.innerHTML = dadosDaTela
+    console.log(dadosDaTela)
+})
+inserirMultiplicacao.addEventListener('click', function(){ 
+    dadosDaTela.push(0)
+    tela.innerHTML = dadosDaTela
+    console.log(dadosDaTela)
+})
+inserirSubtracao.addEventListener('click', function(){ 
+    dadosDaTela.push(0)
+    tela.innerHTML = dadosDaTela
+    console.log(dadosDaTela)
+})
+inserirAdição.addEventListener('click', function(){ 
+    dadosDaTela.push(0)
+    tela.innerHTML = dadosDaTela
+    console.log(dadosDaTela)
+})
+inserirVirgula.addEventListener('click', function(){ 
+    dadosDaTela.push(0)
+    tela.innerHTML = dadosDaTela
+    console.log(dadosDaTela)
+})
+Calcular.addEventListener('click', function(){ 
+    dadosDaTela.push(0)
+    tela.innerHTML = dadosDaTela
+    console.log(dadosDaTela)
+})
