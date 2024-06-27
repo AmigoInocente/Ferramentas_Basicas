09fc4b3390bece5914a25edb965f3d914e30293e
ALISSON GUILHERME FORMENTO <alisson_formento@estudante.sesisenai.org.br>
2024-06-27 16:09:43 -0300

    Mostrei e centralizei os numeros tela, depois adicionei o botão do teclado ao 0

--- calculadora.js
+++ calculadora.js
@@ -1,83 +1,92 @@
 let tela = document.getElementById('tela')
-
 let dadosDaTela = []
+
 inserir0.addEventListener('click', function(){ 
-    dadosDaTela.push(0)
+    dadosDaTela.push("0")
     tela.innerHTML = dadosDaTela
     console.log(dadosDaTela)
 })
+
+inserir0.addEventListener("keydown", function(evento) {
+    const teclaPressionada = evento.key;
+  
+    if (teclaPressionada === "0") { // Exemplo: tecla "0"
+      dadosDaTela.push("0");
+      tela.innerHTML = dadosDaTela;
+      console.log(dadosDaTela);
+    }
+  });
 inserir1.addEventListener('click', function(){ 
-    dadosDaTela.push(0)
+    dadosDaTela.push("1")
     tela.innerHTML = dadosDaTela
     console.log(dadosDaTela)
 })
 inserir2.addEventListener('click', function(){ 
-    dadosDaTela.push(0)
+    dadosDaTela.push("2")
     tela.innerHTML = dadosDaTela
     console.log(dadosDaTela)
 })
 inserir3.addEventListener('click', function(){ 
-    dadosDaTela.push(0)
+    dadosDaTela.push("3")
     tela.innerHTML = dadosDaTela
     console.log(dadosDaTela)
 })
 inserir4.addEventListener('click', function(){ 
-    dadosDaTela.push(0)
+    dadosDaTela.push("4")
     tela.innerHTML = dadosDaTela
     console.log(dadosDaTela)
 })
 inserir5.addEventListener('click', function(){ 
-    dadosDaTela.push(0)
+    dadosDaTela.push("5")
     tela.innerHTML = dadosDaTela
     console.log(dadosDaTela)
 })
 inserir6.addEventListener('click', function(){ 
-    dadosDaTela.push(0)
+    dadosDaTela.push("6")
     tela.innerHTML = dadosDaTela
     console.log(dadosDaTela)
 })
 inserir7.addEventListener('click', function(){ 
-    dadosDaTela.push(0)
+    dadosDaTela.push("7")
     tela.innerHTML = dadosDaTela
     console.log(dadosDaTela)
 })
 inserir8.addEventListener('click', function(){ 
-    dadosDaTela.push(0)
+    dadosDaTela.push("8")
     tela.innerHTML = dadosDaTela
     console.log(dadosDaTela)
 })
 inserir9.addEventListener('click', function(){ 
-    dadosDaTela.push(0)
+    dadosDaTela.push("9")
     tela.innerHTML = dadosDaTela
     console.log(dadosDaTela)
 })
 inserirDivisao.addEventListener('click', function(){ 
-    dadosDaTela.push(0)
+    dadosDaTela.push("/")
     tela.innerHTML = dadosDaTela
     console.log(dadosDaTela)
 })
 inserirMultiplicacao.addEventListener('click', function(){ 
-    dadosDaTela.push(0)
+    dadosDaTela.push("*")
     tela.innerHTML = dadosDaTela
     console.log(dadosDaTela)
 })
 inserirSubtracao.addEventListener('click', function(){ 
-    dadosDaTela.push(0)
+    dadosDaTela.push("-")
     tela.innerHTML = dadosDaTela
     console.log(dadosDaTela)
 })
 inserirAdição.addEventListener('click', function(){ 
-    dadosDaTela.push(0)
+    dadosDaTela.push("+")
     tela.innerHTML = dadosDaTela
     console.log(dadosDaTela)
 })
 inserirVirgula.addEventListener('click', function(){ 
-    dadosDaTela.push(0)
+    dadosDaTela.push(".")
     tela.innerHTML = dadosDaTela
     console.log(dadosDaTela)
 })
 Calcular.addEventListener('click', function(){ 
-    dadosDaTela.push(0)
     tela.innerHTML = dadosDaTela
     console.log(dadosDaTela)
 })
