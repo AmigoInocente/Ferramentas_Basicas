be90c3a5dc791f687024f52c3533e304e8fc541d
Alisson Formento <alisson.formento.tj@gmail.com>
2024-11-08 21:40:22 -0300

    inserindo animação da foto de perfil

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const meuObservatorio = new IntersectionObserver((entradas) => {
-    console.log(entradas);
+   entradas.forEach((entrada) => {
+       if(entrada.isIntersecting){
+            entrada.target.classList.remove('esconder')
+           entrada.target.classList.add('mostrar')
+       }else{
+           entrada.target.classList.remove('mostrar')
+           entrada.target.classList.add('esconder')
+       }
+   })
 })
 
 const elements = document.querySelectorAll('.esconder');
 
 elements.forEach((observatorio) => meuObservatorio.observe(observatorio))
+
+const transicaoInicial = new IntersectionObserver((entradas) => {
+    entradas.forEach((entrada) => {
+        if(entrada.isIntersecting){
+            entrada.target.classList.add('aparecerNome')
+            entrada.target.classList.remove('mostrar')
+            entrada.target.classList.remove('esconder')
+        }
+    })
+ })
+ 
+ const foto = document.querySelectorAll('.fotoDePerfil');
+ 
+ foto.forEach((observatorio) => transicaoInicial.observe(observatorio))
